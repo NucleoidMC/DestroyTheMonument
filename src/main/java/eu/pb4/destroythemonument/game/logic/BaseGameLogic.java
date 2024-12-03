70a9415f36d260a43cf658fad5a403ef3b411ad8
Patbox <patbox4@outlook.com>
2024-12-03 21:28:24 +0100

    Update to 1.21.4

--- src/main/java/eu/pb4/destroythemonument/game/logic/BaseGameLogic.java
+++ src/main/java/eu/pb4/destroythemonument/game/logic/BaseGameLogic.java
@@ -637,7 +637,7 @@ public abstract class BaseGameLogic {
                 float green = ((float) (color / 256) % 256) / 256;
                 float red = ((float) color / 65536) / 256;
 
-                this.gameSpace.getPlayers().sendPacket(new ParticleS2CPacket(new DustParticleEffect(ColorHelper.fromFloats(0, red, green, blue), 0.8f), false, monument.pos.getX() + 0.5d, monument.pos.getY() + 0.5d, monument.pos.getZ() + 0.5d, 0.2f, 0.2f, 0.2f, 0.01f, 5));
+                this.gameSpace.getPlayers().sendPacket(new ParticleS2CPacket(new DustParticleEffect(ColorHelper.fromFloats(0, red, green, blue), 0.8f), false, false, monument.pos.getX() + 0.5d, monument.pos.getY() + 0.5d, monument.pos.getZ() + 0.5d, 0.2f, 0.2f, 0.2f, 0.01f, 5));
             }
         }
 
